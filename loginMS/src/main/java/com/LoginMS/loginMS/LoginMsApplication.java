3a5b165c8fdf0c9d557fc5b10d9b933a47cd42bf
Abhilasha <abhilashakumari.886@gmail.com>
2022-08-29 12:42:17 +0530

    Project initialization

---
+++ loginMS/src/main/java/com/LoginMS/loginMS/LoginMsApplication.java
@@ -0,0 +1,13 @@
+package com.LoginMS.loginMS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoginMsApplication.class, args);
+	}
+
+}
